122f09a3a6783632b356417f5b0adab458536e63
Shubham <shubhamkharche2003@gmail.com>
2025-06-29 18:55:51 +0530

    Guest.java code started and modified

--- src/com/hotel_management_system/dao/Guest.java
+++ src/com/hotel_management_system/dao/Guest.java
@@ -1,4 +1,47 @@
 package com.hotel_management_system.dao;
 
+import com.hotel_management_system.db.DbConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Guest {
+    public void GuestDao(int guest_id,String name,String phone,String email,int choice) throws ClassNotFoundException, SQLException,InterruptedException {
+        DbConnection dbConnect= new DbConnection();
+        Connection connection = dbConnect.dbConnection();
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        String query;
+        switch(choice){
+            case 1:
+                query = "INSERT INTO guests(guest_id,name,phone,email) VALUES(?,?,?,?)";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setInt(1,guest_id);
+                preparedStatement.setString(2,name);
+                preparedStatement.setString(3,phone);
+                preparedStatement.setString(4,email);
+                int row = preparedStatement.executeUpdate();
+                Thread.sleep(2000);
+                if(row>0){
+                    System.out.println("Guest was Added Successfully");
+                }
+                else{
+                    System.out.println("Guest cannot added");
+                }
+
+            case 2:
+                query = "SELECT * FROM guests";
+                preparedStatement = connection.prepareStatement(query);
+                resultSet = preparedStatement.executeQuery();
+                if(resultSet.next()){
+                    System.out.println("+----------+--------------+----------------------+----------------+");
+                    System.out.printf("| %-8s | %-12s | %-20s | %-14s |\n", "guest_id", "name", "phone", "email");
+                    System.out.println("+----------+--------------+----------------------+----------------+");
+                    System.out.printf("| %-8s | %-12s | %-20s | %-14s |\n", resultSet.getInt(guest_id), resultSet.getString(name), resultSet.getString(phone), resultSet.getString(email));
+
+                }
+        }
+    }
 }
